603d98939166979f5673a9d93bb3512648ef2d9f
Evan Truesdale <kayakcat@truecrew.net>
2020-02-05 15:44:04 -0800

    Setup parameter handling, working on header error

---
+++ yee_haw/handlers/getExponent.js
@@ -0,0 +1,17 @@
+"use strict"
+
+function getExponent(req, res) {
+  try {
+
+    var integer = parseInt(req.params.integer, 10)
+    var exponent = integer*integer
+    res.status(200).send({integer: integer, exponent: exponent})
+
+  } catch (err) {
+
+    res.status(500).send({error: 'something blew up'})
+
+  }
+}
+
+module.exports = {getExponent}
--- yee_haw/index.js
+++ yee_haw/index.js
@@ -3,11 +3,17 @@
 const express = require("express")
 const app = express()
 
+const getExponent = require('./handlers/getExponent')
+
 const address = process.env.ADRR || ":3000"
 const [host, port] = address.split(":")
 
 app.get('/', (req, res) =>
   res.send("Hello World!")
+)
+
+app.get('/exponent/:integer', (req, res) =>
+  res.send(getExponent.getExponent(req, res))
 )
 
 app.listen(port, () =>
