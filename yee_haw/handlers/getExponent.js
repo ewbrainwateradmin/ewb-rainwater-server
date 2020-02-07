4e7f10b5c2868c5d55a21f257ac57cb17d0fa824
Jake Harris <jacobharris@Jacobs-MacBook-Pro.local>
2020-02-06 19:41:21 -0800

    Removed anonymous function notation

--- yee_haw/handlers/getExponent.js
+++ yee_haw/handlers/getExponent.js
@@ -5,11 +5,11 @@ function getExponent(req, res) {
 
     var integer = parseInt(req.params.integer, 10)
     var exponent = integer*integer
-    return res.status(200).send({integer: integer, exponent: exponent})
+    res.status(200).send({integer: integer, exponent: exponent})
 
   } catch (err) {
 
-    return res.status(500).send({error: 'something blew up'})
+    res.status(500).send({error: 'something blew up'})
 
   }
 }
--- yee_haw/index.js
+++ yee_haw/index.js
@@ -5,16 +5,14 @@ const app = express()
 
 const getExponent = require('./handlers/getExponent')
 
-const address = process.env.ADRR || ":3000"
+const address = process.env.ADRR || ":8080"
 const [host, port] = address.split(":")
 
 app.get('/', (req, res) =>
   res.send("Hello World!")
 )
 
-app.get('/exponent/:integer', (req, res) =>
-  getExponent.getExponent(req, res)
-)
+app.get('/exponent/:integer', getExponent.getExponent(req, res))
 
 app.listen(port, () =>
   console.log(`Listening on ${host}:${port}`)
