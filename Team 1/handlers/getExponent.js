195ad863a044805cd52b4a8fe54ce03a21807054
Michael Duan <michael@Michaels-MacBook-Air.local>
2020-02-21 23:56:20 -0800

    fix syntax a little

--- Team 1/handlers/getExponent.js
+++ Team 1/handlers/getExponent.js
@@ -1,11 +1,13 @@
-module.export = function getExponent(req, res) {
-    try {
-        var int = parseInt(req.params.integer, 10)
-        var exponent = int * int
-        res.status(200)
-        res.send({"integer": int, "exponent": exponent})
-    } catch (error) {
-        res.status(500);
-        res.send({"error" : "eRroR ErroR ErROr"})
+module.exports = {
+    getExponent: function(req, res){
+        try {
+            var int = parseInt(req.params.integer, 10)
+            var exponent = int * int
+            res.status(200)
+            res.send({"integer": int, "exponent": exponent})
+        } catch (error) {
+            res.status(500);
+            res.send({"error" : "eRroR ErroR ErROr"})
+        }
     }
-}
+};
