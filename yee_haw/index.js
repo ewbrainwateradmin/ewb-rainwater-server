fb0367abdd2e070d12f7caf16b05ac7127387722
Evan Truesdale <kayakcat@truecrew.net>
2020-03-05 18:40:53 -0800

    Working on merge conflict?

--- yee_haw/index.js
+++ yee_haw/index.js
@@ -3,12 +3,16 @@
 const express = require("express")
 const app = express()
 
+const getExponent = require('./handlers/getExponent')
+
 const address = process.env.ADRR || ":80"
 const [host, port] = address.split(":")
 
 app.get('/', (req, res) =>
   res.send("Hello World!")
 )
+
+app.get('/exponent/:integer', getExponent.getExponent)
 
 app.listen(port, () =>
   console.log(`Listening on ${host}:${port}`)
