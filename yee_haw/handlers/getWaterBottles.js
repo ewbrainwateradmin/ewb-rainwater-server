81c5357c0fd5e30bbc62a296cfadb7d934c48800
Evan Truesdale <kayakcat@truecrew.net>
2020-05-04 18:20:35 -0700

    added APIs for the amount of water in bottles and dollars saved

--- yee_haw/handlers/getWaterBottles.js
+++ yee_haw/handlers/getWaterBottles.js
@@ -8,7 +8,12 @@ function getWater(req, res) {
     //Read data from text file
     fs.readFile(path.resolve(__dirname, "water.txt"), (err, data) => {
       if (err) throw err
-      res.status(200).send({water_flow: parseFloat(data.toString())})
+
+      // Using 16 fl.oz. as a "water bottle"
+      
+      var bottles = parseInt(data.toString())*8
+      res.set('Content-Type', 'application/json')
+      res.status(200).send({'bottles_saved': bottles})
     })
 
   } catch (err) {
--- yee_haw/index.js
+++ yee_haw/index.js
@@ -4,7 +4,9 @@ const express = require("express")
 const app = express()
 
 const getExponent = require('./handlers/getExponent')
-const getWater = require('./handlers/getWater')
+const getWaterGallons = require('./handlers/getWaterGallons')
+const getWaterBottles = require('./handlers/getWaterBottles')
+const getWaterDollars = require('./handlers/getWaterDollars')
 const update_from_database = require('./handlers/update_from_database')
 
 const address = process.env.ADRR || ":80"
@@ -12,13 +14,15 @@ const [host, port] = address.split(":")
 
 // Update information from database every 30 minutes (time is in milliseconds)
 update_from_database.update()
-setInterval(update_from_database.update, 30*60*1000)
+setInterval(update_from_database.update, 5*1000)
 
 app.get('/', (req, res) =>
   res.send("Hello World!")
 )
 
-app.get('/current_water_flow', getWater.getWater)
+app.get('/current_water_flow', getWaterGallons.getWater)
+app.get('/current_water_bottles', getWaterBottles.getWater)
+app.get('/current_dollars', getWaterDollars.getWater)
 
 app.listen(port, () =>
   console.log(`Listening on ${host}:${port}`)
