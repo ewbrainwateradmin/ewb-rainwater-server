52ac03780adbf4952fa1790b663a211597f7239e
Evan Truesdale <kayakcat@truecrew.net>
2020-11-28 10:59:34 -0800

    Fully operational public files system

--- server/index.js
+++ server/index.js
@@ -9,14 +9,13 @@ const getWaterDollars = require('./handlers/getWaterDollars')
 const update_from_database = require('./handlers/update_from_database')
 const database_functions = require('./handlers/database_functions')
 
-const address = process.env.ADRR || ":80"
-const [host, port] = address.split(":")
+const port = process.env.PORT || "80"
 
 // Update information from database every 30 minutes (time is in milliseconds)
 update_from_database.update()
 setInterval(update_from_database.update, 30*60*1000)
 
-app.get('/', (req, res) => {res.send("Hello World")})
+app.use(express.static("public"))
 
 app.get('/current_water_flow', getWaterGallons.getWater)
 app.get('/current_water_bottles', getWaterBottles.getWater)
@@ -27,5 +26,5 @@ app.get('/water_records', database_functions.selectAll)
 app.get('/water_records/start/:start_time/end/:end_time', database_functions.selectDate)
 
 app.listen(port, () =>
-  console.log(`Listening on ${host}:${port}`)
+  console.log(`Listening on :${port}`)
 )
